e0c4fa20095439235f312edddbe78139edbe77e6
ziaurjewelhbd-max <ziaurjewelhbd@gmail.com>
2026-05-05 10:27:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
